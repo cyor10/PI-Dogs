17b09d46396a13ddc5631d773a8a8fce7df8f87f
cyor10 <cyor.91@gmail.com>
2023-07-30 18:13:50 -0500

    feat(components): create Landing page

---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import dogImage from '../../img/australian.png'
+import style from './Landing.module.css'
+import { Link } from 'react-router-dom'
+
+export default function Landing() {
+    return (
+        <div className={style.landing}>
+            <img src={dogImage}></img>
+            <div className={style.container}>
+                <h1>¡Esta es una web<br />hecha con amor!</h1>
+                <p>A continuación visualizaras un listado de perros,<br />procura no perder la calma con tanta ternura.</p>
+                <Link to={`/home`}>
+                    <button>Explorar</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
