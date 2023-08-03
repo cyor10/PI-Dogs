2894438b893d02334ba7fff1256bed7e1cbd243b
cyor10 <cyor.91@gmail.com>
2023-08-03 10:37:10 -0500

    feat(components): adjust card component

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import style from '../Card/Card.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Card({ dog }) {
-    const { image, name, temperament, weight } = dog
+    const { id, image, name, temperament, weight } = dog
     return (
         <div className={style.card}>
-            <img src={image} alt='A dog' />
-            <h1>{name}</h1>
-            <p>{temperament}</p>
-            <p>{weight} kg</p>
+            <Link to={`/detail/${id}`} className={style.link}>
+                <img src={image} alt='A dog' />            
+                <h1>{name}</h1>
+                <p>{temperament}</p>
+                <p>{weight} kg</p>
+            </Link>
         </div>
     )
 }
