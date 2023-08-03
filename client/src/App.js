56a6d6435fc55ca5f4e9c63fd7eb80ec654460f7
cyor10 <cyor.91@gmail.com>
2023-08-03 11:21:49 -0500

    feat(src): adjust styles and routes in app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter as Router} from 'react-router-dom'
 import Landing from './components/Landing/Landing'
 import Home from './components/Home/Home'
 import Form from './components/Form/Form'
+import CardDetail from './components/CardDetail/CardDetail';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={<Landing />} />
           <Route path="/home" element={<Home />} />
           <Route path="/create" element={<Form /> }/>
+          <Route path="/detail/:id" element={<CardDetail />} />
         </Routes>
       </div>
     </Router>
